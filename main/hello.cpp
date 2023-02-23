af65fb4d44a9e7572a9416a8d729a05c9d875375
Deekshitha Kumari <90741712+deekshithaprabhakar@users.noreply.github.com>
2023-02-23 21:42:34 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "SRN: PES1UG20CS713\n";
+  cout << "NAME: P DEEKSHITHA KUMARI\n";
   return 0;
 }
